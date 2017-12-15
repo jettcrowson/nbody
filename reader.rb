dbcc40fdb23be79be81d7085d0c773f0d4bed804
Jett Crowson <jettcrowson@gmail.com>
2017-12-15 11:08:08 -0700

    Add comments

--- reader.rb
+++ reader.rb
@@ -12,24 +12,46 @@ module Reader
 
             #Read the file line by line
             f.each_line.with_index do |line, i|
+                
+                #Skip blank lines
                 if line == "\n" then next end
 
                 #Since we only need special action on the first and second lines we can specify that and use the else property to read n number or bodies
                 case i
+
+                    #First line is always the number of bodies
                     when 0
+
                         number_of_bodies = line
+                    
+                    #Second line is always the radius
                     when 1
+
                         radius = line
                     else
+
+                        #Strip all the whitespace to just leave a value for each element
                         body_properties = line.gsub(/\s+/m, ' ').strip.split(" ")
 
+                        #Skip if blank
                         if body_properties[0] == nil then next end
+                        
+                        #End if it goes to the credits
                         if body_properties[0] == "Creator" then break end
+                        if body_properties[0] == "//" then break end
+
+                        #Create a new body with all the properties from the split line
                         bodies.push(Body.new(body_properties[0], body_properties[1], body_properties[2], body_properties[3], body_properties[4], body_properties[5]))
+
                 end
+
             end
+
         end
+
+        #Return a simulation with the list of bodies
         return Simulation.new(number_of_bodies, radius, bodies)
+    
     end
 
 end
--- simulation.rb
+++ simulation.rb
@@ -1,33 +1,56 @@
 require "gosu"
 require_relative "z_order"
 
+#Created by the reader
 class Simulation
 
     attr_reader :number_of_bodies, :radius, :bodies
 
     def initialize(number_of_bodies, radius, bodies)
+
         @number_of_bodies = number_of_bodies.to_i
+
         @radius = radius.to_f
+
         @bodies = bodies
+
     end
 
+    #Scale down the universe scale to fit on the screen
     def convert_coords(x, y, img)
+        
         return [((320 * x) / radius) + 320 - (img.width / 2),
                 ((320 * y) / radius) + 320 - (img.height / 2)]
+    
     end
 
     def update
+
         bodies.each do |body|
+
+            #Each time update is called we need to update the coordinates according to body forces
             body.set_coords(bodies)
+
         end
+
     end
 
     def draw
+
+        #Draw each body
         bodies.each do |body|
+            
+            #Set the image
             body_image = Gosu::Image.new("images/#{body.pic}")
+            
+            #Converted the coords, but invert the y due to gosus inverted y
             coords = convert_coords(body.x, -body.y, body_image)
+            
+            #Finally draw with the scaled down coords
             body_image.draw(coords[0], coords[1], ZOrder::Body)
+
         end
+
     end
 
 
