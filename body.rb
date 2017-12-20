146b0628646733c69b41c43737821c4426a2677a
Jett Crowson <jettcrowson@gmail.com>
2017-12-20 10:30:45 -0700

    Add z direction

--- body.rb
+++ body.rb
@@ -4,22 +4,33 @@ SimulationSpeed = 25000
 #Each planet or object
 class Body
 
-    attr_accessor :x, :y, :vx, :vy, :mass, :pic, :force, :g, :ax, :ay, :t
+    attr_accessor :x, :y, :z, :vx, :vy, :vz, :mass, :pic, :force, :g, :ax, :ay, :t, :radius, :scale
 
     def initialize(x, y, vx, vy, mass, pic)
     
         @x = x.to_f
         @y = y.to_f
         
+        #Starts at 0 because we are not given a starting z
+        @z = 0
+        
         @vx = vx.to_f
         @vy = vy.to_f
+
+        #We are not given a starting z velocity
+        @vz = 0
         
         @mass = mass.to_f
         
         @pic = pic
         
-        #X force and Y Force
-        @force = [0.0, 0.0]
+        #X force and Y Force and Z force
+        @force = [0.0, 0.0, 0.0]
+        
+        #Radius is set in simulation, calculated by the image width to the screen width
+        @radius = 0.0
+
+        @scale = 1
     
     end
 
@@ -32,9 +43,13 @@ class Body
         #Set Y
         self.force[1] = f[1]
 
+        #Set Z
+        self.force[2] = f[2]
+
     end
 
     #Total force, not directional
+    #After further research, stays the same, regardless of z
     def calc_force(m1, m2, r)
 
         #Gravitational constant
@@ -45,18 +60,21 @@ class Body
     end
 
     #Convert the force to directional force, to be called once for x and once for y
+    #Can actually just call this a third time for Z
     def calc_force_directional(f, d, r)
 
         return f * (d / r)
     
     end
 
+    #Apparently does not have to be changed, just called again for the z direction
     def calc_acceleration(f, m)
 
         return f / m
 
     end
 
+    #Again, does not have to be changed
     def calc_velocity(a, v0)
 
         return (a * SimulationSpeed) + v0
@@ -71,6 +89,7 @@ class Body
     end
 
     #Simple distances (How far away up/down are they? Left/right?)
+    #Adapt for Z
     def calc_how_far(b1,b2)
 
         #X distance
@@ -78,25 +97,33 @@ class Body
         
         #Y distance
         dy = (b2.y - b1.y)
+
+        #Z distance
+        dz = (b2.z - b1.z)
         
-        return [dx, dy]
+        return [dx, dy, dz]
     
     end
 
     #Simple pythagoran theorum
+    #adapt for z
     def calc_length(b1, b2)
 
         #Get distances
         d = calc_how_far(b1,b2)
 
-        return Math.sqrt(d[0] ** 2 + d[1] ** 2)
+        xy_length = Math.sqrt(d[0] ** 2 + d[1] ** 2)
+
+        total_length = Math.sqrt(xy_length ** 2 + d[2] ** 2)
+
+        return total_length
     
     end
 
     #Find the force of the body versus all other bodies
     def calc_total_force(bodies)
 
-        f, fx, fy, dx, dy, r = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        f, fx, fy, dx, dy, dz, r = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
         
         #A loop to compare each body
         bodies.each do |body2|
@@ -107,9 +134,10 @@ class Body
             #The distance between the two bodies by straight line
             r = calc_length(self, body2)
 
-            #Specific x and y distances
+            #Specific x, y, and z distances
             dx = calc_how_far(self, body2)[0]
             dy = calc_how_far(self, body2)[1]
+            dz = calc_how_far(self, body2)[2]
 
             #Calculate nondirectional force for the two bodies
             f = calc_force(mass, body2.mass, r)
@@ -117,13 +145,14 @@ class Body
             #Break it up into x and y forces
             fx += calc_force_directional(f, dx, r)
             fy += calc_force_directional(f, dy, r)
+            fz += calc_force_directional(f, dz, r)
 
             #Reset the general force for the next body to compare, but not the directional forces
             f = 0.0
 
         end
 
-        return [fx, fy]
+        return [fx, fy, fz]
     
     end
 
@@ -136,18 +165,22 @@ class Body
         #Set out initial velocities so we can get the current velocities
         v0x = vx
         v0y = vy
+        v0z = vz
 
         #X and Y acceleration
         ax = calc_acceleration(force[0], mass)
         ay = calc_acceleration(force[1], mass)
+        az = calc_acceleration(force[2], mass)
 
         #Set current velocities
         self.vx = calc_velocity(ax, v0x)
         self.vy = calc_velocity(ay, v0y)
+        self.vy = calc_velocity(az, v0z)
 
         #Finally add the distances moved to their x and y to be drawn
         self.x += calc_distance(v0x, vx)
         self.y += calc_distance(v0y, vy)
+        self.z += calc_distance(v0z, vz)
         
     end
 
--- simulation.rb
+++ simulation.rb
@@ -1,4 +1,5 @@
 require "gosu"
+require "body.rb"
 require_relative "z_order"
 
 #Created by the reader
@@ -14,13 +15,21 @@ class Simulation
 
         @bodies = bodies
 
+        bodies.each do |body|
+
+            #should scale the initial radius to be a ratio of img size to screen size and universe radius
+            body.radius = (pic.width * 2 * radius) / 320
+            
+        end
+
     end
 
     #Scale down the universe scale to fit on the screen
-    def convert_coords(x, y, img)
+    def convert_coords(x, y, z, img)
         
         return [((320 * x) / radius) + 320 - (img.width / 2),
-                ((320 * y) / radius) + 320 - (img.height / 2)]
+                ((320 * y) / radius) + 320 - (img.height / 2),
+                ((320 * z) / radius)]
     
     end
 
@@ -33,21 +42,81 @@ class Simulation
 
         end
 
+        check_collisions
+
+    end
+
+    def check_collisions()
+
+        bodies.each do |body|
+
+            bodies.each do |body2|
+
+                if body == body2 then next end
+
+                #if it is colliding in all 3 directions
+                if body.x > body2.x && (body.x +  2 * body2.radius) < (body2.x +  2 * body2.radius) && body.y > body2.y && (body.y +  2 * body2.radius) < (body2.y +  2 * body2.radius) && body.z > body2.z && (body.z +  2 * body2.radius) < (body2.z +  2 * body2.radius)
+                    break_body(body)
+                    break_body(body2)
+                end
+
+            end
+            
+        end
+
+    end
+
+    def break_body(body)
+        bodies.delete(body)
+
+        b1 = Body.new(body.x, body.y, body.vx, body.vy, body.mass / 2, body.pic)
+        b1.scale *= 0.5
+        b1.vx = Math.rand(-1..1)
+        b1.vy = Math.rand(-1..1)
+        b1.vz = Math.rand(-1..1)
+
+        b2 = Body.new(body.x, body.y, body.vx, body.vy, body.mass / 2, body.pic)
+        b2.scale *= 0.5
+        b2.vx = Math.rand(-1..1)
+        b2.vy = Math.rand(-1..1)
+        b2.vz = Math.rand(-1..1)
+
+        bodies.push(b1)
+        bodies.push(b2)
     end
 
     def draw
 
         #Draw each body
-        bodies.each do |body|
+        bodies.each_with_index do |body, i|
+
+            puts "Body #{i + 1} position: x: #{body.x} | y: #{body.y} | z: #{body.z} | x vel: #{body.vx} | y vel: #{body.vy} | z vel: #{body.vz}"
+
+            #Skip if its out of the z radius
+            if body.z.abs > radius then next end
             
             #Set the image
             body_image = Gosu::Image.new("images/#{body.pic}")
             
             #Converted the coords, but invert the y due to gosus inverted y
-            coords = convert_coords(body.x, -body.y, body_image)
+            coords = convert_coords(body.x, -body.y, body.z body_image)
+
+            #how far in the z range is it
+            scale_percent = (body.z / radius)
+
+            #Body scale used for when the bodies split and are half size
+            scale = body.scale
+
+            if scale_percent > 0
+                #If the z is positive
+                scale = scale * scale_percent * 5
+            else
+                #If negative
+                scale = scale * scale_percent * 0.2
+            end
             
             #Finally draw with the scaled down coords
-            body_image.draw(coords[0], coords[1], ZOrder::Body)
+            body_image.draw(coords[0], coords[1], convert_coords[2], scale)
 
         end
 
